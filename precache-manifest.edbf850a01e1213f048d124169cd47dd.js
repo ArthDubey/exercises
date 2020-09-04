0c90bc9ae673bc80f2524ad8966612f09c6daad6
Arth Dubey <46257062+ArthDubey@users.noreply.github.com>
2020-09-04 09:43:29 +0530

    Add files via upload

---
+++ precache-manifest.edbf850a01e1213f048d124169cd47dd.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d6547c2635a55e8370dbc79b3b44866",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b1783a7c30db7912d64",
+    "url": "/static/css/2.1c944ba7.chunk.css"
+  },
+  {
+    "revision": "52c734d2917e0072411a",
+    "url": "/static/css/main.fe570634.chunk.css"
+  },
+  {
+    "revision": "0b1783a7c30db7912d64",
+    "url": "/static/js/2.9a95cdc4.chunk.js"
+  },
+  {
+    "revision": "662609caf1afbba122fff71d51eaacd5",
+    "url": "/static/js/2.9a95cdc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52c734d2917e0072411a",
+    "url": "/static/js/main.6964defe.chunk.js"
+  },
+  {
+    "revision": "97c3bf4fa058deebf56e",
+    "url": "/static/js/runtime-main.f41137ac.js"
+  },
+  {
+    "revision": "c219535ce73beda629ff3c29d788cc6f",
+    "url": "/static/media/first.c219535c.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "aca88bc18b3be8bc21238da208707e00",
+    "url": "/static/media/logo.aca88bc1.png"
+  },
+  {
+    "revision": "63eae674f617309b255efcb5433206ff",
+    "url": "/static/media/menu.63eae674.svg"
+  },
+  {
+    "revision": "7a15da1075b3a310f2a213a07eac6797",
+    "url": "/static/media/second.7a15da10.jpg"
+  }
+]);
